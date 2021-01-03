3e28b68ed28ddf0e23ab8455d6b2578cf9d49c50
Hemal Buha <buhahemal10@gmail.com>
2021-01-03 09:39:20 +0530

    Array / Reverse Array Program Modify(Add For loop Reverse Method)

--- Array/reverse_array.py
+++ Array/reverse_array.py
@@ -1,3 +1,7 @@
 Array1 = ["MCA","BCA","12th","10th"]
+tempReverseArray = []
+for i in range(len(Array1)-1,-1,-1):
+    tempReverseArray.append(Array1[i])
+print("Print Array Using For loop Reverse Array",tempReverseArray)    
 Array1.reverse() # return True or false
 print("Print Array Using Reverse Method ",Array1)
