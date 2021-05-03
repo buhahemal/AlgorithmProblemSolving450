6882f881d33652a8c234c307582d1fff78b97662
Hemal Buha <buhahemal10@gmail.com>
2021-05-03 17:48:50 +0530

    Move ALL negative number one side

--- Array/Move_all_the_negative_elements_to_one_side_of_the_array.py
+++ Array/Move_all_the_negative_elements_to_one_side_of_the_array.py
@@ -1,41 +1,12 @@
-# Python program to Move All -ve Element At End 
-# Without changing order Of Array Element 
-
-# Moves all -ve element to end of array in 
-# same order. 
-def segregateElements(arr, n): 
-	# Create an empty array to store result 
-	temp = [0 for k in range(n)] 
-
-	# Traversal array and store +ve element in 
-	# temp array 
-	j = 0 # index of temp 
-	for i in range(n): 
-		if (arr[i] >= 0 ): 
-			temp[j] = arr[i] 
-			j +=1
-
-	# If array contains all positive or all negative. 
-	if (j == n or j == 0): 
-		return
-
-	# Store -ve element in temp array 
-	for i in range(n): 
-		if (arr[i] < 0): 
-			temp[j] = arr[i] 
-			j +=1
-
-	# Copy contents of temp[] to arr[] 
-	for k in range(n): 
-		arr[k] = temp[k] 
-
-# Driver program 
-arr = [1 ,-1 ,-3 , -2, 7, 5, 11, 6 ] 
-n = len(arr) 
-
-segregateElements(arr, n); 
-
-for i in range(n): 
-	print(arr[i]) 
-
-# Contributed by Afzal aka Saan 
+def rearrange(arr,n):
+	j=0
+	for i in range(0,n):
+		if(arr[i] < 0):
+			temp = arr[i]
+			arr[i] = arr[j]
+			arr[j] = temp
+			j += 1
+	print(arr)
+arr = [-1, 2, -3, 4, 5, 6, -7, 8, 9]
+n = len(arr)
+rearrange(arr, n)
